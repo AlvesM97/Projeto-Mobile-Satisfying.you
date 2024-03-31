6dedc371c6645f20fd441a10c90fcb3e4d7bccc4
AlvesM97 <51722003+AlvesM97@users.noreply.github.com>
2024-03-31 16:35:02 -0300

    Create Botao.js

---
+++ src/components/Botao.js
@@ -0,0 +1,30 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+
+const Botao = (props) => {
+
+    const texto = props.texto
+
+    return (
+        <TouchableOpacity style={estilos.fundo}>
+            <Text style={estilos.texto}>{texto}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const estilos = StyleSheet.create({
+    fundo: {
+        marginTop: 20,
+        backgroundColor: '#37BD6D',
+        height: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    texto: {
+        color: '#FFFFFF',
+        fontSize: 18
+    }
+
+})
+
+export default Botao
