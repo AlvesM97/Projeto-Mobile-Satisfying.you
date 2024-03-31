06693e22d4fa6652837895ee0727f9281d34598c
AlvesM97 <51722003+AlvesM97@users.noreply.github.com>
2024-03-31 16:35:11 -0300

    Create modificarPesquisa.tsx

---
+++ src/views/modificarPesquisa.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Botao from '../components/Botao';
+
+const ModificarPesquisa: React.FC = () => {
+  return (
+    <ScrollView>
+      {/* Parte superior */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => console.log('Botão de volta pressionado')}>
+          <Icon name='arrow-back' size={40} color="#573FBA" />
+        </TouchableOpacity>
+        <Text style={styles.title}>Modificar pesquisa</Text>
+      </View>
+
+      {/* Parte inferior */}
+      <View style={styles.container}>
+        <Text style={styles.label}>Nome</Text>
+        <TextInput
+          value="Carnaval 2024"
+          style={styles.input}
+        />
+
+        <Text style={styles.label}>Data</Text>
+        <TextInput
+          value="16/02/2024"
+          style={styles.input}
+        />
+
+        <Text style={styles.label}>Imagem</Text>
+        <View style={styles.imageContainer}>
+          <Text>Ícone de Imagem</Text>
+        </View>
+
+        {/* Botões */}
+        <Botao texto="SALVAR" onPress={() => console.log('Salvar pressionado')} />
+        {/* Adicione outro botão para "Apagar" de forma semelhante */}
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#2B1D62',
+    padding: 15,
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    marginLeft: 10,
+  },
+  container: {
+    paddingVertical: 10, 
+    paddingHorizontal: 100,
+    backgroundColor: '#372775',
+  },
+  label: {
+    color: 'white',
+    marginTop: 20,
+  },
+  input: {
+    backgroundColor: '#F0F0F0', // Cor de fundo do input
+    height: 40,
+  },
+  imageContainer: {
+    height: 40,
+    backgroundColor: '#F0F0F0', // Cor de fundo da imagem
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ModificarPesquisa;
