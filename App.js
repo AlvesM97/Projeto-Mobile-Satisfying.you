d37e52903f4e4ff490cf10507f1ed167fc0d2392
Gustavo Teodoro <gteodorox@gmail.com>
2024-04-24 01:00:05 -0300

    Home e Ações de Pesquisa

--- App.js
+++ App.js
@@ -1,46 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Home from './src/views/home';
+import acoesPesquisa from './src/views/acoesPesquisa';
+const Stack = createStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-            <Text>Projeto Mobile - Satisfying.you</Text>
-        </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2B1D62', // Define a cor do cabeçalho
+          },
+          headerTintColor: '#fff', // Define a cor dos elementos do cabeçalho
+          headerTitleStyle: {
+            fontSize: 35,
+            fontFamily: 'AveriaLibre-Regular',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTitle: '', // Define o título do cabeçalho como uma string vazia para ocultar
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  /* Implemente a ação do menu aqui */
+                }}>
+                <Icon
+                  style={styles.styleIcon}
+                  name="menu"
+                  size={40}
+                  color="#fff"
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="acoesPesquisa"
+          component={acoesPesquisa}
+          options={{
+            headerTitle: 'Carnaval', // Define o título do cabeçalho como uma string vazia para ocultar
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  /* Implemente a ação do menu aqui */
+                }}>
+                <Icon
+                  style={styles.styleIcon}
+                  name="arrow-back"
+                  size={40}
+                  color="#573FBA"
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
+  styleIcon: {
+    paddingLeft: 20,
+  },
 });
 
 export default App;
---
+++ src/views/acoesPesquisa.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
+
+const Home = () => {
+  // Dados dos eventos
+  const eventos = [
+    {
+      nome: 'Modificar',
+      imagem: require('../imagens/modificar.png'),
+    },
+    {
+      nome: 'Coletar Dados',
+      imagem: require('../imagens/coletarDados.png'),
+    },
+    {
+      nome: 'Relatório',
+      imagem: require('../imagens/relatorio.png'),
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.gridContainer}>
+        <View style={styles.cardContainer}>
+          {eventos.map((evento, index) => (
+            <TouchableOpacity key={index} style={styles.card}>
+              <Image source={evento.imagem} style={styles.cardImage} />
+              <Text style={styles.cardText}>{evento.nome}</Text>
+              <Text style={styles.cardDate}>{evento.data}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#372775',
+    flex: 1,
+    paddingHorizontal: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  card: {
+    backgroundColor: '#312464',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+    width: '30%', // Ajuste a largura dos cards conforme necessário
+  },
+  cardImage: {
+    width: 80,
+    height: 80,
+    marginBottom: 10,
+  },
+  cardText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+  cardDate: {
+    fontSize: 14,
+    color: '#888',
+    marginTop: 5,
+  },
+});
+
+export default Home;
