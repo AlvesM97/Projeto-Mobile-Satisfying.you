e08def3042d320c3fcba1c151e6a2d134614983f
Henrique Moraes <rique_moraes@hotmail.com>
2024-08-17 14:19:54 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -6,10 +6,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-    resolver: {
-        sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
-    },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
